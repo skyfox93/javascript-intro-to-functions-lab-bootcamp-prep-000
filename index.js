f7f024219a20ad3e70305e3f6d8409885c0570ba
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 03:05:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string){ return string.toUpperCase();}
-function whisper(string){ string.toLowerCase();}
+function whisper(string){ return string.toLowerCase();}
 function logshout(string){console.log(shout(string));}
