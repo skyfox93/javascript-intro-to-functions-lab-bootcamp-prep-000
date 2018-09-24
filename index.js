ac9330b72fd280a62a6206dc45c8e9c7f97f70c2
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-24 03:13:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ function shout(string){ return string.toUpperCase();}
 function whisper(string){ return string.toLowerCase();}
 function logShout(string){console.log(shout(string));}
 function logWhisper(string){console.log(whisper(string));}
-function sayHiToGrandma(string){if(string==="I love you, Grandma."){return "I love you too."} else if(string.toUpperCase()===string){return "YES INDEED"} else{return "I can't hear you!";}}
+function sayHiToGrandma(string){if(string==="I love you, Grandma."){return "I love you, too."} else if(string.toUpperCase()===string){return "YES INDEED"} else{return "I can't hear you!";}}
